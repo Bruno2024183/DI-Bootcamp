bcefe851743cd4c3ea89e9cbccb70dfa2e18bc87
Bruno Rodrich <bruno.rodrich@mail.huji.ac.il>
2024-08-05 14:43:58 +0300

    middle of day 1

---
+++ Week1/D1/D1/exerciseXP/Exercises 05.08.2024.py
@@ -0,0 +1,17 @@
+
+
+# Paso 1: Calcular 99^3
+resultado_potencia = 99 **3
+
+# Paso 2: Multiplicar el resultado por 8
+resultado_final = resultado_potencia * 8
+
+# Imprimir el resultado final
+print(resultado_final)
+
+resultado_potencia = 99 ** 3
+resultado_final = resultado_potencia * 8
+print(resultado_final)
+
+
+
