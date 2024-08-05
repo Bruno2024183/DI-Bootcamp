f70d06e0378a9088bcce96f69c30ce1660af2c93
Bruno Rodrich <bruno.rodrich@mail.huji.ac.il>
2024-08-05 16:03:26 +0300

    update

--- Week1/D1/D1/exerciseXP/Exercises 05.08.2024.py
+++ Week1/D1/D1/exerciseXP/Exercises 05.08.2024.py
@@ -15,6 +15,49 @@ print(resultado_final)
 
 print("hello")
 
+#Predict the output of the following code snippets:
+print("5 < 3:",5 < 3)
+3 == 3#true
+print(3 == "3")
+#print("3" > 3)
+
+"Hello" == "hello"
+
+computer_brand="Acer"
+print(f"tengo una computadora {computer_brand}")
 
 
+# Parte 1: Crear una variable llamada name y establece su valor en tu nombre
+name = "Bruno"
 
+# Parte 2: Crea una variable llamada age y estable su valor en tu edad
+age = 33
+
+# Parte 3: Crea una variable llamda shoe_size y establece su valor en tu talla del calzado
+shoe_size = 44
+
+# Parte 4: Crea una variable llamada info y establece su valor en una oracion interesante sobre Ti
+info = f"Me llamo {name}, tengo {age} años y mi talla de calzado es {shoe_size}."
+
+# Parte 5: Imprimir el mensaje de informacion
+print (info)
+
+
+# Crear dos variables, a y b, con valores numericos
+a = 8
+b = 4
+
+# Verificar si a es mayor que b
+if a > b:
+    print("Hello World")
+
+
+# Pedir al usuario un numero
+numero =  input("give me a number\n")
+numero= int(numero)
+# Determinar si el numero es par o impar
+
+if numero % 7 == 0:
+    print("EL numero es par.")
+else:
+    print("El numero es impar.")
