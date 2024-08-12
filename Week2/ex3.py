f467efe1968f6905de0e110ab5c53d936cdde89e
Bruno Rodrich <bruno.rodrich@mail.huji.ac.il>
2024-08-12 11:29:46 +0300

    Upload

--- Week2/ex3.py
+++ Week2/ex3.py
@@ -1,16 +1,15 @@
 class Song:
-    def _init_(self, lyrics):
+    def __init__(self, lyrics):
         self.lyrics = lyrics
 
-def sing_me_a_song(self):
-    for line in self.lyrics:
-        print(line)
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
 
 stairway = Song([
     "There's a lady who's sure",
     "all that glitters is gold",
-    "and she's buying a stairwat to heaven"
-])
+    "and she's buying a stairwat to heaven"])
 
 stairway.sing_me_a_song()
 
