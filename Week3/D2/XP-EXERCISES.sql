5b8c5850cfe3f5d82d6d0fb7a529fc346e811c20
Bruno Rodrich <bruno.rodrich@mail.huji.ac.il>
2024-08-22 00:26:47 +0300

    Upload

--- Week3/D2/XP-EXERCISES.sql
+++ Week3/D2/XP-EXERCISES.sql
@@ -1,42 +1,60 @@
--- Create customer table
-CREATE TABLE customers (
-    customer_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL
-);
+SELECT * FROM customer;
 
--- Insert records into customers table
-INSERT INTO customers (customer_id, first_name, last_name) 
-VALUES 
-(1, 'Greg', 'Jones'),
-(2, 'Sandra', 'Jones'),
-(3, 'Scott', 'Scott'),
-(4, 'Trevor', 'Green'),
-(5, 'Melanie', 'Johnson');
+SELECT first_name || ' ' || last_name AS full_name FROM customer;
 
--- Create items table
-CREATE TABLE items (
-    item_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    price NUMERIC(10, 2) NOT NULL
-);
+SELECT * FROM customer;
 
--- Insert records into items table
-INSERT INTO items (item_id, name, price) 
-VALUES 
-(1, 'Small Desk', 100),
-(2, 'Large Desk', 300),
-(3, 'Fan', 80);
+SELECT first_name || ' ' || last_name AS full_name FROM customer;
 
-SELECT * FROM items;
+SELECT DISTINCT create_date FROM customer;
 
-SELECT * FROM items WHERE price > 80;
+SELECT * FROM customer
+ORDER BY first_name DESC;
 
-SELECT * FROM items WHERE price <= 300;
+SELECT film_id, title, description, release_year, rental_rate 
+FROM film
+ORDER BY rental_rate ASC;
 
-SELECT * FROM customers WHERE last_name = 'Smith';
+SELECT address, phone 
+FROM address
+WHERE district = 'Texas';
 
-SELECT * FROM customers WHERE last_name = 'Jones';
+SELECT * FROM film
+WHERE film_id IN (15, 150);
 
-SELECT * FROM customers WHERE first_name != 'Scott';
+SELECT film_id, title, description, length, rental_rate 
+FROM film
+WHERE title = 'My Favorite Movie';
 
+SELECT film_id, title, description, length, rental_rate 
+FROM film
+WHERE title LIKE 'My%';
+
+SELECT film_id, title, rental_rate 
+FROM film
+ORDER BY rental_rate ASC
+LIMIT 10;
+
+SELECT film_id, title, rental_rate 
+FROM film
+ORDER BY rental_rate ASC
+OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
+
+SELECT c.first_name, c.last_name, p.amount, p.payment_date
+FROM customer c
+JOIN payment p ON c.customer_id = p.customer_id
+ORDER BY c.customer_id;
+
+SELECT f.film_id, f.title
+FROM film f
+LEFT JOIN inventory i ON f.film_id = i.film_id
+WHERE i.film_id IS NULL;
+
+SELECT ci.city, co.country 
+FROM city ci
+JOIN country co ON ci.country_id = co.country_id;
+
+SELECT p.customer_id, c.first_name, c.last_name, p.amount, p.payment_date, p.staff_id
+FROM payment p
+JOIN customer c ON p.customer_id = c.customer_id
+ORDER BY p.staff_id
